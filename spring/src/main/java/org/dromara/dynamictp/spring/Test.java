e2bab1e41fc31928319aac49c2475b557644aefa
akun <yk.wu@dface.cn>
2025-02-17 17:16:49 +0800

    feat(spring): 添加测试类 Test
    - 在 spring 模块中新增 Test 类
    - 包含 main 方法，打印 "hello dynamic-tp"

---
+++ spring/src/main/java/org/dromara/dynamictp/spring/Test.java
@@ -0,0 +1,13 @@
+package org.dromara.dynamictp.spring;
+
+/**
+ * @author akun
+ * @create 2025-02-17 下午5:14
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello dynamic-tp");
+    }
+}
